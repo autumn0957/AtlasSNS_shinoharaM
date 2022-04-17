fca6ed3f560b1dc499638fe9729b2e281a6a9383
DESKTOP-9CC5GTC\81908 <natsu0957railgun@yahoo.co.jp>
2022-04-17 20:10:55 +0900

    途中

--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\DB;
-//use DB;
+use App\User;
 
 class PostsController extends Controller
 {
     //
     public function index(){
-        $users = DB::table('users')->get();
+        $users = \DB::table('users')->get();
         return view('posts.index', ['users' => $users]);
     }
 }
--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -7,14 +7,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach ($users as $user) 
-                echo $user->name;
-            }
             <div class="card">
                 <div class="card-header p-3 w-100 d-flex">
-                    <img src="{{ $user->profile_image }}" class="rounded-circle" width="50" height="50">
+                    <img src="{{ $user->images }}" class="rounded-circle" width="50" height="50">
                     <div class="ml-2 d-flex flex-column">
-                        <p class="mb-0">{{ $user->name }}</p><!--ユーザー表示させる-->
-                        <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $user->screen_name }}</a>
+                        <p class="mb-0">{{ $user->username }}</p><!--ユーザー表示させる-->
+                        <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $user->user_id }}</a>
                     </div>
                 </div>
             </div>
