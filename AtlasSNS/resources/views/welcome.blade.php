492b7929b76c65602af64cc91684d68b10abddab
DESKTOP-9CC5GTC\81908 <natsu0957railgun@yahoo.co.jp>
2022-04-27 20:04:49 +0900

    ヘッダー/ロゴにトップへ遷移するリンクを設置

--- AtlasSNS/resources/views/welcome.blade.php
+++ AtlasSNS/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   <a href=" {{ url('/top') }} "> Laravel</a>
                 </div>
 
                 <div class="links">
